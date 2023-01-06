f36ae0908f89488a3ae9c4d7484588f57fe3ac97
Prathik Rajendran M <prathik.rajendran@go-jek.com>
2023-01-06 14:49:29 +0530

    add archimedes method

--- src/pi.rs
+++ src/pi.rs
@@ -3,14 +3,11 @@ extern crate redis_module;
 
 use redis_module::{Context, RedisError, RedisResult, RedisString};
 
-fn pi_approx(_: &Context, args: Vec<RedisString>) -> RedisResult {
+fn pi_archimedes(_: &Context, args: Vec<RedisString>) -> RedisResult {
     if args.len() != 2 {
         return Err(RedisError::WrongArity);
     }
-
-    let mut result = 0.0;
-    let mut sign = 1.0;
-    
+   
     let times_arg = args[1].parse_integer();
 
     if times_arg.is_err() {
@@ -18,6 +15,47 @@ fn pi_approx(_: &Context, args: Vec<RedisString>) -> RedisResult {
     }
 
     let times = times_arg.unwrap();
+
+    if times <= 0 {
+        return Err(RedisError::String("Wrong iteration count".to_string()));
+    }
+
+    if times > 16 {
+        return Err(RedisError::String("More than 16 iterations not supported".to_string()));
+    }
+
+    let mut poly_len_squared:f64 = 2.0;
+    let mut poly_sides = 4;
+    
+    for _i in 1..times {
+        poly_len_squared = 2.0 - 2.0*(1.0 - poly_len_squared/4.0).sqrt();
+        poly_sides *= 2;
+    }
+
+    let result = poly_sides as f64 * poly_len_squared.sqrt() / 2.0;
+    Ok(redis_module::RedisValue::Float(result))
+
+}
+
+fn pi_gregory(_: &Context, args: Vec<RedisString>) -> RedisResult {
+    if args.len() != 2 {
+        return Err(RedisError::WrongArity);
+    }
+   
+    let times_arg = args[1].parse_integer();
+
+    if times_arg.is_err() {
+        return Err(RedisError::WrongType);
+    }
+
+    let times = times_arg.unwrap();
+
+    if times <= 0 {
+        return Err(RedisError::String("Wrong iteration count".to_string()));
+    }
+
+    let mut result = 0.0;
+    let mut sign = 1.0;
 
     for d in 0..times {
         let df = d as f64;
@@ -37,6 +75,7 @@ redis_module! {
     version: 1,
     data_types: [],
     commands: [
-        ["pi", pi_approx, "", 0, 0, 0],
+        ["pi.gregory", pi_gregory, "", 0, 0, 0],
+        ["pi.archimedes", pi_archimedes, "", 0, 0, 0],
     ],
 }
